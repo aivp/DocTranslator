3987f548c0f9ca587e32ff6bdd2da21df7d3d904
yishu <9661202@qq.com>
2025-12-04 19:24:40 +0800

    前端文件名修复

---
+++ frontend/src/components/Filing.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- 版权信息 -->
+  <div class="filing">
+    <p>© 2025 DocTranslator 1.4.0 保留所有权利</p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.filing {
+  width: 100%;
+  font-size: 14px;
+  text-align: center;
+  padding: 20px 0;
+  box-sizing: border-box;
+  line-height: 20px;
+  p {
+    color: #949db0;
+    margin: 0;
+  }
+}
+</style>
